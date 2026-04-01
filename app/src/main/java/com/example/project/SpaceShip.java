8e95f1886bb5ca312bedef99bb8f37ea7592ad11
Your Name <your.email@example.com>
2026-04-01 12:11:44 +0300

    added more new classes and base functions without logic

--- app/src/main/java/com/example/project/SpaceShip.java
+++ app/src/main/java/com/example/project/SpaceShip.java
@@ -1,4 +1,29 @@
 package com.example.project;
 
 public class SpaceShip {
+    private CrewQuarters quarters;
+    private PassengerManifest manifest;
+    private TrainingRoom trainingRoom;
+    private int daysOnBoard = 1;
+    private int shipHealth = 100;
+
+    public void runTrainingRoom() {
+
+    }
+
+    public void runMissionRoom() {
+
+    }
+
+    public void checkManifest() {
+
+    }
+
+    public void checkCrewQuarters() {
+
+    }
+
+    public Character onboardCrewMember() {
+
+    }
 }
