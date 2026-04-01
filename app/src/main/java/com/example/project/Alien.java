3973a297c60df102a0ea750d68816d4fa97bdc1a
fabled03 <mattklaughlin@gmail.com>
2026-04-01 13:20:26 +0300

    alien.java, medic.java, missionroom.jave, parasite.jave, trainingroom.java added base methods without functionality

---
+++ app/src/main/java/com/example/project/Alien.java
@@ -0,0 +1,24 @@
+package com.example.project;
+
+public class Alien extends Threat {
+    private int maxHealth;
+    private int currentHealth;
+    private String name;
+    private int exp;
+
+
+    public Alien(int maxHealth, int currentHealth, String name, int exp) {
+        super(maxHealth, currentHealth, name, exp);
+
+    }
+
+    @Override
+    public int attack() {
+    }
+
+    @Override
+    public int special() {
+        //shoots laser that does AOE
+    }
+
+}
---
+++ app/src/main/java/com/example/project/Medic.java
@@ -0,0 +1,23 @@
+package com.example.project;
+
+public class Medic extends Character {
+
+    private int maxHealth;
+    private int currentHealth;
+    private String name;
+    private int exp;
+    private int missionCompleted;
+    //representive of final blows, can be added in a different way to uml
+    private int finalBlows;
+
+    public Medic(int maxHealth, int currentHealth, String name, int exp, int missionCompleted, int finalBlows){
+
+    }
+    public int attack(){
+
+    }
+    public int special(){
+
+    }
+    //Medic will be able to heal other characters
+}
