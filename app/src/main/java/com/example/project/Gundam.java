5767199a3f9b94111c25a4dcabfc4d8e5ad15e59
fabled03 <mattklaughlin@gmail.com>
2026-04-01 13:59:49 +0300

    see if this works

---
+++ app/src/main/java/com/example/project/Gundam.java
@@ -0,0 +1,32 @@
+package com.example.project;
+
+
+public class Gundam extends Threat{
+    private int maxHealth;
+    private int currentHealth;
+    private String name;
+    private int exp;
+    private boolean missileUsed;
+
+
+    public Gundam(int maxHealth, int currentHealth, String name, int exp, boolean missileUsed) {
+        super(maxHealth, currentHealth, name, exp);
+        this.missileUsed = missileUsed;;
+    }
+
+    public int attack() {
+
+    }
+
+    public int special() {
+        // shoots a missile for big damage
+    }
+
+    public void reloadMissile() {
+
+    }
+
+    public void setMissileUsed(boolean state) {
+
+    }
+}
