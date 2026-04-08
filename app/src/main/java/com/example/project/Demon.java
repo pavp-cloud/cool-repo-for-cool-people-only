b6d4620fd714bbf21554876153cd5ffaa0956737
pavp-cloud <pavp0277@gmail.com>
2026-04-08 13:52:41 +0300

    did more of the logic for the specific threat classes, specifically a universal basic attack; did the special for Pirate, check comments; did the special for Demon;

--- app/src/main/java/com/example/project/Demon.java
+++ app/src/main/java/com/example/project/Demon.java
@@ -1,5 +1,7 @@
 package com.example.project;
 
+import java.util.Random;
+
 public class Demon extends Threat{
 
     private int maxHealth;
@@ -8,6 +10,9 @@ public class Demon extends Threat{
     private int exp;
     private int buffStackCounter;
 
+    //CONSTANTS
+
+    private final int baseAttack = 2;
     public Demon(int maxHealth, int currentHealth, String name, int exp, int buffStackCounter) {
         super(maxHealth, currentHealth, name, exp);
         this.buffStackCounter = buffStackCounter;
@@ -15,11 +20,25 @@ public class Demon extends Threat{
     }
     @Override
     public int attack(Character character1, Character character2) {
+        //basic base attack logic, can be copied everywhere but with different scaling
+        //this guy gets extra buff scaling
+        Random random = new Random();
+        int target = random.nextInt(2); //returns int from 0 to 1 (higher bound is exclusive, so a bound of 2 gives a 1)
+        int damage = (int)(baseAttack*getBuffStackCounter() + (this.getExp() * 0.5));
 
+        if (target == 0) {
+            character1.takeDamage(damage);
+        }
+        else {
+            character2.takeDamage(damage);
+        }
+        return 0;
     }
 
     @Override
     public int special(Character character1, Character character2) {
+        incrementBuffStackCounter();
+        return 0;
         // gains a buff to damage
     }
 
@@ -33,7 +52,6 @@ public class Demon extends Threat{
 
     @Override
     public void takeDamage(int damage) {
-
+        this.currentHealth += damage;
     }
 }
-//hello daddy
--- app/src/main/java/com/example/project/Gundam.java
+++ app/src/main/java/com/example/project/Gundam.java
@@ -1,12 +1,18 @@
 package com.example.project;
 
 
+import java.util.Random;
+
 public class Gundam extends Threat{
     private int maxHealth;
     private int currentHealth;
     private String name;
     private int exp;
     private boolean missileUsed = false;
+
+    //CONSTANTS
+
+    private final int baseAttack = 2;
 
 
     public Gundam(int maxHealth, int currentHealth, String name, int exp) {
@@ -15,12 +21,26 @@ public class Gundam extends Threat{
 
     @Override
     public int attack(Character character1, Character character2) {
+        //basic base attack logic, can be copied everywhere but with different scaling
+        Random random = new Random();
+        int target = random.nextInt(2); //returns int from 0 to 1 (higher bound is exclusive, so a bound of 2 gives a 1)
+        int damage = (int)(baseAttack + (this.getExp() * 0.5));
 
+        if (target == 0) {
+            character1.takeDamage(damage);
+        }
+        else {
+            character2.takeDamage(damage);
+        }
+        return 0;
     }
 
     @Override
     public int special(Character character1, Character character2) {
-        // shoots a missile for big damage
+        //shoots a missile for big damage
+
+
+        return 0;
     }
 
     public void reloadMissile() {
@@ -33,6 +53,6 @@ public class Gundam extends Threat{
 
     @Override
     public void takeDamage(int damage) {
-
+        this.currentHealth += damage;
     }
 }
