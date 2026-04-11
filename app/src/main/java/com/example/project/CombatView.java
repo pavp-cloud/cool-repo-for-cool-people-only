9235b63baa38ea073cd5b64d1d28299ddfd50043
fabled03 <mattklaughlin@gmail.com>
2026-04-11 20:54:40 +0300

    Updates to animation

--- app/src/main/java/com/example/project/CombatView.java
+++ app/src/main/java/com/example/project/CombatView.java
@@ -1,7 +1,5 @@
 package com.example.project;
 
-import static android.provider.SyncStateContract.Helpers.update;
-
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -10,6 +8,7 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 public class CombatView extends SurfaceView implements Runnable {
+
     public enum combatState{
         Players_Turn,
         Enemys_Turn,
@@ -28,13 +27,14 @@ public class CombatView extends SurfaceView implements Runnable {
     private Canvas canvas;
     private Paint paint;
     private int animationTimer = 0;
-    private boolean showExplosion = false;
+    private boolean showSpecialAttack = false;
     private float exploisonX, explosionY;
     private boolean showBasicAttack = false;
-    private float swingX, swingY;
+    private float shootX, shootY;
 
 
-    public CombatView(Context context) {
+
+    public CombatView (Context context) {
         super(context);
         this.surfaceHolder = getHolder();
         this.paint = new Paint();
@@ -50,21 +50,33 @@ public class CombatView extends SurfaceView implements Runnable {
             control();//controls frame rate
         }
     }
-    private void update(){
-        //Function as the logic to move/control the characters and actions by turns 
+
+
+    private void update() {
+        //Function as the logic to move/control the characters and actions by turns
         if (activeCombat == combatState.Players_Turn) {
             //wait for input from player
-            }
-        else if (activeCombat == combatState.Enemys_Turn){
+        } else if (activeCombat == combatState.Enemys_Turn) {
             perfromEnemyActions();
             activeCombat = combatState.Players_Turn;
             //sends back to players turn
-            }
-        if (activeCombat == combatState.Animating){
+        }
+        if (activeCombat == combatState.Animating) {
+            animationTimer++;
 
+            if (selectAttack == attackState.Attack) {
+                if (animationTimer > 50) endAnimation();
+            } else if (selectAttack == attackState.Special_Attack) {
+                if (animationTimer > 80) endAnimation();
+            }
         }
-        }
-    private void perfromEnemyActions(){
+    }
+    private void endAnimation(){
+        animationTimer = 0;
+        activeCombat = combatState.Enemys_Turn;
+        selectAttack = null;
+    }
+    private static void perfromEnemyActions(){
         //Enemy AI logic
     }
 
@@ -79,6 +91,23 @@ public class CombatView extends SurfaceView implements Runnable {
 
             surfaceHolder.unlockCanvasAndPost(canvas);
         }
+        if (activeCombat == combatState.Animating){
+            if (selectAttack == attackState.Attack){
+                paint.setColor(Color.RED);
+                float laserX = 300 + (animationTimer % 10) * 40;
+                canvas.drawRect(laserX, 500, laserX + 20, 505, paint);
+
+            }
+            else if (selectAttack == attackState.Special_Attack){
+                int alpha = Math.max(0, 255 - (animationTimer * 255));
+                paint.setAlpha(alpha);
+                paint.setColor(Color.rgb(20,69, 255));
+                canvas.drawCircle(800, 500, animationTimer * 4, paint);
+
+            }
+
+        }
+        surfaceHolder.unlockCanvasAndPost(canvas);
     }
     private void control(){
         try {
