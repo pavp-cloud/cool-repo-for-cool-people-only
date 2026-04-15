0db5cf6fca16900726ee39033fd5b0408b6b0d43
Your Name <your.email@example.com>
2026-04-15 15:50:03 +0300

    created base training room fragment file.

---
+++ app/src/main/java/com/example/project/TrainingRoomFragment.java
@@ -0,0 +1,26 @@
+package com.example.project;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TrainingRoomFragment extends Fragment {
+
+    public TrainingRoomFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // PAVEL PUT CODE HERE
+
+
+
+
+        return inflater.inflate(R.layout.fragment_training_room, container, false);
+    }
+}
