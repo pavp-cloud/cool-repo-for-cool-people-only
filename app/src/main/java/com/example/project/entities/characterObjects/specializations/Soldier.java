2c4093a0c4d998fc0cf65f8bb791c4112d2e6ec4
heikkikornmann-bit <heikki.kornmann@gmail.com>
2026-04-18 11:51:35 +0300

    changed the grenade method of the soldier to be reusable but hurt himself

--- app/src/main/java/com/example/project/entities/characterObjects/specializations/Soldier.java
+++ app/src/main/java/com/example/project/entities/characterObjects/specializations/Soldier.java
@@ -8,25 +8,16 @@ import com.example.project.entities.entityInterfaces.CombatCharacterSpecial;
 import com.example.project.entities.threatObjects.Threat;
 
 public class Soldier extends Character implements CombatActor, CombatCharacterSpecial {
-    private boolean usedGrenade = false;
 
     //CONSTANTS
     private final int baseAttack = 5;
     private final int grenadeAttack = 10;
     private final double attackScaling = 0.5;
-    private final double specialScaling = 0.8;
-    private final double damageVulnerability = 1.0;
+    private final double specialScaling = 1.5;
+    private final double damageVulnerability = 0.9;
 
     public Soldier(int maxHealth, int currentHealth, String name, int exp){
         super(maxHealth, currentHealth, name, exp);
-    }
-
-    public boolean getUsedGrenade() {
-        return this.usedGrenade;
-    }
-
-    public void setUsedGrenade(boolean state) {
-        this.usedGrenade = state;
     }
 
     public int attack(){
@@ -35,23 +26,18 @@ public class Soldier extends Character implements CombatActor, CombatCharacterSp
 
     //"throws" the grenade
     //its like attack, but with a higher base value
-    //could also be higher scaling but thats details
-    //todo: if we're going for the "reusing a one-time use action reloads it" thing we're gonna have to communicate that on the ui just fyi
+    // The attack hits himself as well because he is in the bvlast radius
     public int special(){
-        if (usedGrenade) {
-            setUsedGrenade(false);
-            return 0;
-        } else {
-            setUsedGrenade(true);
-            return (int) (grenadeAttack + (this.getExp() * specialScaling));
-        }
+        int damage = (int) (grenadeAttack + (this.getExp() * specialScaling));
+        int selfDamage = (int) (damage * 0.4);
+        takeDamage(selfDamage);
+        return damage;
     }
 
     public void endOfCombatPrep(Threat threat) {
         gainExp(threat.getExp());
         missionsCompleted++;
         increaseMaxHealth(threat.getExp());
-        setUsedGrenade(false);
     }
 
     public void takeDamage(int attackIntensity) {
